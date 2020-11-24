d5d662ca3dc5440ce6f59ab7beaf0a0bec51ff7e
LittleWorms <49356972+LittleWorms@users.noreply.github.com>
2020-11-24 09:10:47 +0700

    SwiftlyOfSixty Version 7
    SwiftlyOfSixty Version 7

--- SwiftlyOfSixty/src/swiftlyofsixty/Vocabulary.java
+++ SwiftlyOfSixty/src/swiftlyofsixty/Vocabulary.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Vocabulary {
 
-    public String[] wordEasy = {"dog", "cat", "fat", "owl", "old", "low", "bad", "jaw", "jam", "sit", "bee", "key", "she", "him", "her", "eye", "sex", "die", "six", "one", "two", "cry", "pie", "pig", "bag", "end", "home", "mos", "ant", "raw"};
+    private String[] wordEasy = {"dog", "cat", "fat", "owl", "old", "low", "bad", "jaw", "jam", "sit", "bee", "key", "she", "him", "her", "eye", "sex", "die", "six", "one", "two", "cry", "pie", "pig", "bag", "end", "home", "mos", "ant", "raw"};
     private String[] wordNormal = {"clean", "adult", "dance", "earth", "alarm", "brain", "block", "fight", "group", "human", "face", "japan", "laugh", "metal", "nurse", "ocean", "ball", "peter", "queen", "rabbit", "shirt", "table", "onion", "unity", "voice", "woman", "youth", "zebra", "young", "music"};
     private String[] wordHard = {"supreme", "advance", "keyboard", "cocacola", "assault", "antibody", "benefit", "capital", "tranfer", "factory", "gallery", "husband", "imagine", "justice", "crewmate", "library", "machine", "nuclear", "organic", "lazyloxy", "xxxtentacion", "linkingpark", "bohemian", "crystal", "summary", "nestle", "trouble", "unusual", "whether", "imposter"};
     private int wordAmount = 30;
@@ -15,7 +15,6 @@ public class Vocabulary {
         GameMenu game = new GameMenu();
         switch (mode) {
             case 1:
-                System.out.println("111");
                 selectedMode = wordEasy;
                 swiftly.gameStart();
                 break;
